3ddddc956eae4c79e1a01485480b293227ca79d5
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-11 11:43:20 +0600

    chore: change port

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '116.68.200.97:6051',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
