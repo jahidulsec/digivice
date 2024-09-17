e19e4cddd976be2273288defc1bd8fb904325e97
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-17 11:26:38 +0600

    chore: adding express and additional fonts

---
+++ client/src/app/admin/layout.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/admin/home/Header';
+
+export const dynamic = 'force-dynamic';
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'client', 'public')))
+
+app.listen(port, () => {
+    console.log("Server listening on PORT", port)
+})
