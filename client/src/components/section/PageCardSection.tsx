8d241f608e3b24a8840526529392b6d02e398345
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-18 16:35:26 +0600

    styles: page card container

--- client/src/components/section/PageCardSection.tsx
+++ client/src/components/section/PageCardSection.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function PageCardSection({children}: {children: React.ReactNode}) {
   return (
-    <section className="relative page-content-section flex items-center justify-center min-h-screen z-10">
+    <section className="relative page-content-section flex items-center justify-center min-h-screen py-10 z-10">
         <div className=" bg-p2 border-2 rounded-md border-white w-[80vw] min-h-[80vh]">
           <div className="relative flex justify-center mt-10 w-full opacity-10">
             <Image src={bgQ} width={180} height={180} alt='Q'  /> 
