380482cf357aa66cfee4a139e55466cb9cd769ef
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-19 15:11:22 +0600

    bug: small fix in folder

--- client/src/app/(viewers)/doctor/[name]/home/[folder]/page.tsx
+++ client/src/app/(viewers)/doctor/[name]/home/[folder]/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import React from 'react';
 import FolderPageHeader from '@/components/folder/Header';
-import { cache } from '@/lib/cache';
 import db from '../../../../../../../db/db';
 import ContentSection from '@/components/folder/ContentSection';
 import { MessageSquareOff } from 'lucide-react';
@@ -13,27 +12,18 @@ export const metadata: Metadata = {
 };
 
 async function FolderPage({ params }: { params: { name: string; folder: string } }) {
-  
-  const getFolderName = cache(() => {
+  const getFolderName = () => {
     return db.folder.findUnique({
-      where: {id: Number(params.folder)},
-      select: {name: true}
-    })
-  }, 
-  [`/doctor/${params.name}/home/${params.folder}`, 'getDoctorFolderContent'],
-)
-  
-  const getDoctorFolderContent = cache(
-    () => {
-      return db.folderContent.findMany({ where: { folderId: Number(params.folder) } });
-    },
-    [`/doctor/${params.name}/home/${params.folder}`, 'getDoctorFolderContent'],
-  );
+      where: { id: Number(params.folder) },
+      select: { name: true },
+    });
+  };
 
-  const [folderContent, folder] = await Promise.all([
-    getDoctorFolderContent(),
-    getFolderName()
-  ])
+  const getDoctorFolderContent = () => {
+    return db.folderContent.findMany({ where: { folderId: Number(params.folder) } });
+  };
+
+  const [folderContent, folder] = await Promise.all([getDoctorFolderContent(), getFolderName()]);
 
   return (
     <>
@@ -44,7 +34,7 @@ async function FolderPage({ params }: { params: { name: string; folder: string }
         <PageCardSection>
           <FolderPageHeader folderName={folder?.name as string} />
           <div className="px-6 flex mb-14 flex-col gap-14">
-            {folderContent.length > 0.0 ? (
+            {folderContent.length > 0 ? (
               <ContentSection folderContent={folderContent} />
             ) : (
               <div className="flex justify-center items-center flex-col py-20 text-p1/50 pointer-events-none">
