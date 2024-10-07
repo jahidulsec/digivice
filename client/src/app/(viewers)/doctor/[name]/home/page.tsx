a290fce8996485016ec9e664a8ffee79259b7146
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-10-07 17:02:02 +0600

    feat: only show folder that have content

--- client/src/app/(viewers)/doctor/[name]/home/page.tsx
+++ client/src/app/(viewers)/doctor/[name]/home/page.tsx
@@ -16,7 +16,14 @@ export const metadata: Metadata = {
 
 export default async function DoctorHome({ params }: { params: { name: string } }) {
   const [folders, socialLinks] = await Promise.all([
-    db.folder.findMany({ where: { doctor: { slug: params.name } } }),
+    db.folder.findMany({
+      where: { doctor: { slug: params.name } },
+      include: {
+        _count: {
+          select: { folderContent: true },
+        },
+      },
+    }),
     db.socialMediaLinks.findMany({ where: { doctor: { slug: params.name } } }),
   ]);
 
@@ -27,36 +34,40 @@ export default async function DoctorHome({ params }: { params: { name: string } 
 
       <PageCardSection>
         <div className="button-container px-6 my-14 flex flex-col gap-14">
-          {folders.map((item) => (
-            <Button
-              key={item.id}
-              name={item.name}
-              folderId={item.id.toString()}
-              src={
-                item.name.toLowerCase() === 'video'
-                  ? videoIcon
-                  : item.name.toLowerCase() === 'infographic'
-                    ? infoIcon
-                    : item.name.toLowerCase() === 'pdf'
-                      ? pdfIcon
-                      : folderIcon
-              }
-            />
-          ))}
+          {JSON.stringify(folders)}
+          {folders.map((item) => {
+            if (item._count.folderContent == 0) return null 
+            return (
+              <Button
+                key={item.id}
+                name={item.name}
+                folderId={item.id.toString()}
+                src={
+                  item.name.toLowerCase() === 'video'
+                    ? videoIcon
+                    : item.name.toLowerCase() === 'infographic'
+                      ? infoIcon
+                      : item.name.toLowerCase() === 'pdf'
+                        ? pdfIcon
+                        : folderIcon
+                }
+              />
+            );
+          })}
         </div>
 
         <section className="links flex items-center justify-center gap-3 mb-5">
           {socialLinks.map((item) => (
-            <ButtonUi asChild variant={'outline'} size={'icon'}>
-              <Link target="_blank" href={`https://${item.url}`} className="text-p1 rounded-full border-p1">
+            <ButtonUi asChild variant={'outline'} size={'icon'} className="rounded-full">
+              <Link target="_blank" href={`https://${item.url}`} className="text-p1  border-p1">
                 {item.siteName.toLowerCase() === 'facebook' ? (
-                  <Facebook />
+                  <Facebook className="size-4" />
                 ) : item.siteName.toLowerCase() === 'instagram' ? (
-                  <Instagram />
+                  <Instagram className="size-4" />
                 ) : item.siteName.toLowerCase() === 'twiter' ? (
-                  <Twitter />
+                  <Twitter className="size-4" />
                 ) : (
-                  <LinkIcon />
+                  <LinkIcon className="size-4" />
                 )}
               </Link>
             </ButtonUi>
