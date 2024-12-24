7489056cd7c352e40ca4be9f20ca79807c0ee25b
Jahidul Islam <jahidul.app@gmail.com>
2024-12-24 12:16:43 +0600

    chore: adding csp in next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,16 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   crossOrigin: 'anonymous',
   images: {
@@ -6,6 +19,19 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
   },
 };
 
